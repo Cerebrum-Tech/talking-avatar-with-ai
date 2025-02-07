cf4dc23207abf8ff3b2210dc517068186693538d
CaptainSP <19982002ke@gmail.com>
2025-02-07 15:01:07 +0300

    Add button to SecondVideo component for language selection; update audio message duration and mouth cues

--- apps/frontend/src/components/SecondVideo.tsx
+++ apps/frontend/src/components/SecondVideo.tsx
@@ -1,11 +1,28 @@
+import { MdStart } from "react-icons/md";
+
 export default function SecondVideo() {
   return (
     <>
-      <video className="w-screen h-screen" autoPlay loop muted onClick={()=> {
-            location.href = "/pick-language";
-        }}>
+      <video
+        className="w-screen h-screen"
+        autoPlay
+        loop
+        muted
+        onClick={() => {
+          location.href = "/pick-language";
+        }}
+      >
         <source src="second-video.mp4" type="video/mp4" />
       </video>
+      <button
+        onClick={() => {
+          location.href = "/pick-language";
+        }}
+        className="fixed left-1/2 top-2/3 transform -translate-x-1/2 -translate-y-1/2 text-3xl z-10 m-auto border bg-white text-[#ff5656] rounded-full p-4 px-4 font-semibold uppercase flex items-center gap-2 justify-around"
+      >
+        <MdStart size={50} className="text-[#ff5656] etxt-3xl" />
+        Başla
+      </button>
     </>
   );
 }
